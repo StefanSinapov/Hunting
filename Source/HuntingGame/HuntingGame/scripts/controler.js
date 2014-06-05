fd2abae3562bd83a22274b2824f5ef49803fd409
phristov <pavelh@abv.bg>
2014-06-06 00:06:39 +0300

    Added controller.

---
+++ Source/HuntingGame/HuntingGame/scripts/controler.js
@@ -0,0 +1,32 @@
+function Controler(renderer, blaze) {
+    var s = renderer.stage;
+
+    s.on("click", function () {
+
+        var mousePos = s.getPointerPosition();
+        var x = mousePos.x;
+        var y = mousePos.y;
+        var coordinate = new Coordinate(x, y);
+
+        console.log("Click move " + coordinate.toString());
+    })
+
+    s.on("mousemove", function () {
+
+        var mousePos = s.getPointerPosition();
+        var x = mousePos.x;
+        var y = mousePos.y;
+        var coordinate = new Coordinate(x, y);
+
+        console.log("Mouse move " + coordinate.toString());
+    })
+}
+
+
+Controler.prototype.getMouseCoordinates = function () {
+    // var mousePosition = this.getMouseCoordinates();
+    // var x = mousePosition.x;
+    // var y = mousePosition.y;
+    // var position = new Coordinate(x,y);
+    // return position;
+}
