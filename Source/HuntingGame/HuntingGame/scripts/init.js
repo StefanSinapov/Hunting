6402382ecbb2a90e56994e7fd8bdd534dae61025
phristov <pavelh@abv.bg>
2014-06-09 20:00:48 +0300

    Moved constants to game.js

--- Source/HuntingGame/HuntingGame/scripts/init.js
+++ Source/HuntingGame/HuntingGame/scripts/init.js
@@ -6,16 +6,6 @@ function init() {
     game.start();
 }
 
-var CONFIG = (function() {
-    var private = {
-        'WIDTH': 800,
-        'HEIGHT': 600
-    };
-
-    return {
-        get: function(name) { return private[name]; }
-    };
-})();
 
 /*
  *  On window load function.
