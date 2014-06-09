44184fcbe3c8781aafe0b33d632c3e9f3ad928fe
phristov <pavelh@abv.bg>
2014-06-09 16:18:59 +0300

    Added function getRandomInt and comments to game.js.

--- Source/HuntingGame/HuntingGame/scripts/game.js
+++ Source/HuntingGame/HuntingGame/scripts/game.js
@@ -22,33 +22,41 @@ function Game() {
  */
 Game.prototype.start = function () {
     var INITIAL_WAIT_TIME = 100;
+
     var blazeInitialCoordinate = new Coordinate(this.width / 2, this.width / 2);
-    var blaze = new Blaze(blazeInitialCoordinate);
+    var blaze = new Blaze(blazeInitialCoordinate); // blaze object
 
-    var renderer = new Renderer(this.width, this.height);
+    var renderer = new Renderer(this.width, this.height); // renderer object
+    var eggmen = []; // array to hold eggmen.
 
-    var controller = new Controller();
-   // renderer.drawAll(blaze);
+    var controller = new Controller(); // controller object
+    // renderer.drawAll(blaze);
 
     setTimeout(function () {
-        animationLoop(renderer, controller, blaze);
+        animationLoop(renderer, controller, blaze, eggmen);
     }, INITIAL_WAIT_TIME);
-
-
 };
 
 /*
-*   Function for animation loop of the game.
+ *   Function for animation loop of the game.
  */
-function animationLoop(renderer, controller, blaze) {
+function animationLoop(renderer, controller, blaze, eggmen) {
 
     if (controller.mousePosition !== undefined) {
         blaze.position = controller.mousePosition;
     }
 
     requestAnimFrame(function () {
-        animationLoop(renderer, controller, blaze);
+        animationLoop(renderer, controller, blaze, eggmen);
     });
 
     renderer.drawAll(blaze);
 }
+
+/*
+ *   Returns a random integer between min and max
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
