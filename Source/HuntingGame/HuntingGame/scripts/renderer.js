214bc50ec2870b34fc45175c51012e9a882a8e67
phristov <pavelh@abv.bg>
2014-06-09 20:08:45 +0300

    Added constants to renderer.js.

--- Source/HuntingGame/HuntingGame/scripts/renderer.js
+++ Source/HuntingGame/HuntingGame/scripts/renderer.js
@@ -1,5 +1,5 @@
 /*
- *   Renderer object responsible for drawing gama objects on the canvas.
+ *   Initializes a new instance of the Renderer class.
  */
 function Renderer(width, height) {
 
@@ -14,8 +14,6 @@ function Renderer(width, height) {
     document.body.appendChild(canvas);
 
     this.ctx = canvas.getContext('2d');
-
-    // this.myCanvas = new MyCanvas(this.ctx);
 }
 
 /*
@@ -32,26 +30,46 @@ Renderer.prototype.drawAll = function (blaze, eggman) {
 };
 
 /*
+ *   Constants for the game object.
+ */
+Renderer.CONFIG = function () {
+    var private = {
+        'BLAZE_LINE_LENGTH': 60,
+        'BLAZE_INNER_RADIUS': 10,
+        'BLAZE_OUTER_RADIUS': 100,
+        'BLAZE_COLOR':'red'
+    };
+
+    return {
+        get: function (name) {
+            return private[name];
+        }
+    };
+}();
+
+/*
  *   Draws Blaze.
  */
 function _drawBlaze(context, blaze) {
 
-    var LINE_LENGTH = 30;
-    var RADIUS_1 = 20;
-    var RADIUS_2 = 10;
+    var LINE_LENGTH = Renderer.CONFIG.get('BLAZE_LINE_LENGTH');
+    var INNER_RADIUS = Renderer.CONFIG.get('BLAZE_INNER_RADIUS');
+    var OUTER_RADIUS = Renderer.CONFIG.get('BLAZE_OUTER_RADIUS');
+
     var centerX = blaze.position.x;
     var centerY = blaze.position.y;
 
-    var point1X = centerX + LINE_LENGTH;
+    var point1X = centerX + LINE_LENGTH / 2;
     var point1Y = centerY;
 
-    var point2X = centerX - LINE_LENGTH;
+    var point2X = centerX - LINE_LENGTH / 2;
     var point2Y = centerY;
 
     var point3X = centerX;
-    var point3Y = centerY + LINE_LENGTH;
+    var point3Y = centerY + LINE_LENGTH / 2;
+
     var point4X = centerX;
-    var point4Y = centerY - LINE_LENGTH;
+    var point4Y = centerY - LINE_LENGTH / 2;
 
     context.beginPath();
     context.moveTo(point1X, point1Y);
@@ -59,10 +77,10 @@ function _drawBlaze(context, blaze) {
     context.moveTo(point3X, point3Y);
     context.lineTo(point4X, point4Y);
     context.moveTo(centerX, centerY);
-    context.arc(centerX, centerY, RADIUS_1, 0, 2 * Math.PI, false);
+    context.arc(centerX, centerY, INNER_RADIUS, 0, 2 * Math.PI, false);
     context.moveTo(centerX, centerY);
-    context.arc(centerX, centerY, RADIUS_2, 0, 2 * Math.PI, false);
-    context.strokeStyle = 'red';
+    context.arc(centerX, centerY, OUTER_RADIUS, 0, 2 * Math.PI, false);
+    context.strokeStyle = Renderer.CONFIG.get('BLAZE_COLOR');
     context.stroke();
 }
 
@@ -74,13 +92,14 @@ function _drawClip(context, blaze) {
         sx = 20,
         sy = context.canvas.height - BULLET_HEIGHT - 10,
         bulletsCount = blaze.getBulletsCount();
-    
+
     context.beginPath();
-    for (var i = 0; i < bulletsCount; i+=1) {
+    for (var i = 0; i < bulletsCount; i += 1) {
         context.moveTo(sx, sy);
         context.strokeRect(sx, sy, BULLET_WIDHT, BULLET_HEIGHT);
         sx = sx + BULLET_WIDHT + SPACING;
-    };
+    }
+    ;
     context.stroke();
 }
 
