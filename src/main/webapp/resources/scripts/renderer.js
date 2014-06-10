ad656b934b03179ac99a7a690b99752b6e9079c2
phristov <pavelh@abv.bg>
2014-06-10 18:45:14 +0300

    Added function createRandomCloud to renderer.js.

--- src/main/webapp/resources/scripts/renderer.js
+++ src/main/webapp/resources/scripts/renderer.js
@@ -19,7 +19,7 @@ function Renderer(width, height) {
 /*
  *   Draws all objects.
  */
-Renderer.prototype.drawAll = function(blaze, eggman) {
+Renderer.prototype.drawAll = function (blaze, eggman) {
     this.ctx.clearRect(0, 0, this.width, this.height);
     this.drawBlaze(blaze);
     this.drawClip(blaze);
@@ -33,7 +33,7 @@ Renderer.prototype.drawAll = function(blaze, eggman) {
 /*
  *   Constants for the game object.
  */
-Renderer.CONFIG = function() {
+Renderer.CONFIG = function () {
     var constants = {
         BLAZE_LINE_LENGTH: 60,
         BLAZE_INNER_RADIUS: 10,
@@ -49,7 +49,7 @@ Renderer.CONFIG = function() {
     };
 
     return {
-        get: function(name) {
+        get: function (name) {
             return constants[name];
         }
     };
@@ -58,7 +58,7 @@ Renderer.CONFIG = function() {
 /*
  *   Draws Blaze.
  */
-Renderer.prototype.drawBlaze = function(blaze) {
+Renderer.prototype.drawBlaze = function (blaze) {
 
     var LINE_LENGTH = Renderer.CONFIG.get('BLAZE_LINE_LENGTH');
     var INNER_RADIUS = Renderer.CONFIG.get('BLAZE_INNER_RADIUS');
@@ -95,7 +95,7 @@ Renderer.prototype.drawBlaze = function(blaze) {
 /*
  *   Draws the clip (available bullets).
  */
-Renderer.prototype.drawClip = function(blaze) {
+Renderer.prototype.drawClip = function (blaze) {
     var sx = 20;
     var sy = this.height - Renderer.CONFIG.get('BULLET_HEIGHT') - sx;
     var bulletsCount = blaze.bullets;
@@ -111,7 +111,7 @@ Renderer.prototype.drawClip = function(blaze) {
 /*
  *   Draws Eggman.
  */
-Renderer.prototype.drawEggman = function(eggman) {
+Renderer.prototype.drawEggman = function (eggman) {
     var eggmanTopLeftX = eggman.position.x;
     var eggmanTopLeftY = eggman.position.y;
     this.ctx.strokeStyle = 'green';
@@ -121,7 +121,7 @@ Renderer.prototype.drawEggman = function(eggman) {
 /*
  *   Creates the background.
  */
-Renderer.prototype.createBackground = function() {
+Renderer.prototype.createBackground = function () {
     // Creates the svg element
     var svg = document.createElementNS(Renderer.CONFIG.get('SVG_NS'), 'svg');
     svg.setAttribute('height', this.height.toString());
@@ -164,11 +164,14 @@ Renderer.prototype.createBackground = function() {
         svg.appendChild(this.createRandomPlanet());
     }
 
+    var cloud = this.createRandomCloud();
+    svg.appendChild(cloud);
+
 
     return svg;
 };
 
-Renderer.prototype.creteCanvas = function() {
+Renderer.prototype.creteCanvas = function () {
     var canvas = document.createElement('canvas');
     canvas.setAttribute('id', 'drawing');
     canvas.setAttribute('width', this.width.toString());
@@ -182,7 +185,7 @@ Renderer.prototype.creteCanvas = function() {
 /*
  *   Creates a random star.
  */
-Renderer.prototype.createRandomStar = function() {
+Renderer.prototype.createRandomStar = function () {
     var polygon = document.createElementNS(Renderer.CONFIG.get('SVG_NS'), 'polygon');
 
     var x = getRandomInt(50, this.width - 50);
@@ -214,7 +217,7 @@ Renderer.prototype.createRandomStar = function() {
 /*
  *   Creates a random planet.
  */
-Renderer.prototype.createRandomPlanet = function() {
+Renderer.prototype.createRandomPlanet = function () {
     var circle = document.createElementNS(Renderer.CONFIG.get('SVG_NS'), 'circle');
     var cx = getRandomInt(50, this.width - 50);
     var cy = getRandomInt(50, this.height - 50);
@@ -229,7 +232,7 @@ Renderer.prototype.createRandomPlanet = function() {
 /*
  *   Draws the score.
  */
-Renderer.prototype.drawScore = function(blaze) {
+Renderer.prototype.drawScore = function (blaze) {
     var scoreText = "Score: " + (blaze.score | 0); //todo: add blaze score
     this.ctx.font = "30px Comic Sans MS, Arial, Sans";
     this.ctx.fillStyle = '#dcdcdc';
@@ -239,14 +242,14 @@ Renderer.prototype.drawScore = function(blaze) {
 /*
  *   Draws the Intro screen.
  */
-Renderer.prototype.drawIntro = function() {
+Renderer.prototype.drawIntro = function () {
 
 };
 
 /*
  *   Draws the Exit screen
  */
-Renderer.prototype.drawExit = function() {
+Renderer.prototype.drawExit = function () {
 
 };
 
@@ -254,52 +257,52 @@ Renderer.prototype.drawB1 = function () {
 
     var paper = Raphael(container, 800, 600);
     var x = 40,
-    y = 40;
+        y = 40;
 
     /*Draw background - sky*/
 
     paper.rect(x, y, 800, 600)
-    .attr({
-        fill: 'lightblue',
-        'stroke-width': 0
-    });
+        .attr({
+            fill: 'lightblue',
+            'stroke-width': 0
+        });
 
     /* Draw sand.*/
 
     paper.rect(x, (y + 400), 800, 200)
-    .attr({
-        fill: '100-darkgoldenrod-gold',
-        opacity: 0.8,
-        'stroke-width': 0
-    });
+        .attr({
+            fill: '100-darkgoldenrod-gold',
+            opacity: 0.8,
+            'stroke-width': 0
+        });
 
     /* Draw sea. */
     paper.rect(x, (y + 350), 800, 50)
-    .attr({
-        fill: '100-blue:2-darkblue:100',
-        opacity: 1,
-        'stroke-width': 3,
-        stroke: '40-blue-darkblue',
-        'stroke-linecap': 'round'
-    });
+        .attr({
+            fill: '100-blue:2-darkblue:100',
+            opacity: 1,
+            'stroke-width': 3,
+            stroke: '40-blue-darkblue',
+            'stroke-linecap': 'round'
+        });
 
     /*
-*   Draw waves.
-*/
+     *   Draw waves.
+     */
     paper.path("M" + (x + 0) + "," + (y + 400)
         + "C" + (x + 50) + "," + (y + 450) + "," + (x + 150) + "," + (y + 400) + "," + (x + 170) + "," + (y + 400)
         + "C" + (x + 170) + "," + (y + 400) + "," + (x + 380) + "," + (y + 470) + "," + (x + 450) + "," + (y + 400)
         + "C" + (x + 450) + "," + (y + 400) + "," + (x + 570) + "," + (y + 440) + "," + (x + 650) + "," + (y + 400)
         + "C" + (x + 650) + "," + (y + 400) + "," + (x + 700) + "," + (y + 450) + "," + (x + 800) + "," + (y + 400)
         + "Z")
-    .attr({
-        fill: '90-white-blue',
-        opacity: 0.9,
-        stroke: '90-#000f48-#ffffff',
-        'stroke-width': 5,
-        'stroke-linecap': 'round',
-        'stroke-linejoin': 'round'
-    });
+        .attr({
+            fill: '90-white-blue',
+            opacity: 0.9,
+            stroke: '90-#000f48-#ffffff',
+            'stroke-width': 5,
+            'stroke-linecap': 'round',
+            'stroke-linejoin': 'round'
+        });
 
     /*Draw cloud.*/
 
@@ -310,15 +313,15 @@ Renderer.prototype.drawB1 = function () {
         + "C" + (x + 620) + "," + (y + 60) + "," + (x + 655) + "," + (y + 50) + "," + (x + 660) + "," + (y + 45)
         + "C" + (x + 675) + "," + (y + 35) + "," + (x + 680) + "," + (y + 45) + "," + (x + 690) + "," + (y + 40)
         + "Z")
-    .attr({
-        fill: '270-white-azure',
-        opacity: 0.2,
-        stroke: '100-gray-white-blue',
-        'stroke-width': 5,
-        'stroke-linecap': 'round',
-        'stroke-linejoin': 'round'
+        .attr({
+            fill: '270-white-azure',
+            opacity: 0.2,
+            stroke: '100-gray-white-blue',
+            'stroke-width': 5,
+            'stroke-linecap': 'round',
+            'stroke-linejoin': 'round'
 
-    });
+        });
 
     /* Draw mountains */
 
@@ -328,18 +331,42 @@ Renderer.prototype.drawB1 = function () {
         + "C" + (x + 400) + "," + (y + 310) + "," + (x + 530) + "," + (y + 280) + "," + (x + 630) + "," + (y + 320)
         + "C" + (x + 630) + "," + (y + 320) + "," + (x + 900) + "," + (y + 20) + "," + (x + 800) + "," + (y + 350)
         + "Z")
-    //paper.path("M"+(x -10) 350 C 130 100 260 340 300 320 C 300 320 350 280 400 310 C 400 310 530 280 630 320 C 630 320 900 20 800 350 Z ')
-    .attr({
-        fill: "100-darkgreen-white",
-        opacity: 0.1,
-        stroke: "100-darkgreen-white",
-        'stroke-width': 5,
-        'stroke-linecap': 'round',
-        'stroke-linejoin': 'round'
-    })
-    /*контролна черта*/
+        //paper.path("M"+(x -10) 350 C 130 100 260 340 300 320 C 300 320 350 280 400 310 C 400 310 530 280 630 320 C 630 320 900 20 800 350 Z ')
+        .attr({
+            fill: "100-darkgreen-white",
+            opacity: 0.1,
+            stroke: "100-darkgreen-white",
+            'stroke-width': 5,
+            'stroke-linecap': 'round',
+            'stroke-linejoin': 'round'
+        })
+   
     paper.path("M" + (x + 50) + "," + (y + 50) + " L" + (x + 100) + "," + (y + 100));
 
+};
+
+/*
+ *   Creates a random cloud;
+ */
+Renderer.prototype.createRandomCloud = function () {
+
+    var cloud = document.createElementNS(Renderer.CONFIG.get('SVG_NS'), 'path');
+    var x = getRandomInt(100, this.width - 200);
+    var y = getRandomInt(100, this.height - 500);
+
+    var points = "M" + (x ) + "," + (y) + "C" + (x + 20) + "," + (y + 30) + "," + (x + 10) + "," + (y + 50) + "," + (x - 10 ) + "," + (y + 40)
+        + "C" + (x - 20) + "," + (y + 35) + "," + (x - 40) + "," + (y + 45) + "," + (x - 50) + "," + (y + 40)
+        + "C" + (x - 70) + "," + (y + 50) + "," + (x - 80) + "," + (y + 40) + "," + (x - 50) + "," + (y + 20)
+        + "C" + (x - 60) + "," + (y + 10) + "," + (x - 45) + "," + (y) + "," + (x - 40) + "," + (y - 5)
+        + "C" + (x - 10) + "," + (y - 15) + "," + (x - 20) + "," + (y - 5) + "," + (x - 10) + "," + (y - 10)
+        + "Z";
+
+    cloud.setAttribute('d', points);
+    var style = "fill: white;opacity: 0.8;"
+    cloud.setAttribute('style', style);
+    console.log(cloud);
+
+    return cloud;
 };
 
 
