989229e16e101258d9d5ee683d774ed268552359
StefanSinapov <stefan.sinapov@gmail.com>
2014-06-10 17:32:14 +0300

    exported blaze update logics into .update method

--- src/main/webapp/resources/scripts/blaze.js
+++ src/main/webapp/resources/scripts/blaze.js
@@ -19,14 +19,18 @@ Blaze.prototype.constructor = Blaze;
 /*
  *   Shoots and lowers the count of the total bullets.
  */
-Blaze.prototype.shoot = function () {
+Blaze.prototype.shoot = function(target) {
     this.bullets--;
-
     if (this.bullets === 0) {
         this.reload();
     }
 
-    //here to check if hit something?
+    if(target.position.x <= this.position.x && this.position.x <= target.position.x + target.width 
+        && target.position.y <= this.position.y && this.position.y <= target.position.y + target.height)
+    {
+        // console.log('hit at x:' + this.position.x + " y:" + this.position.y);
+        // TODO: if target have health call its method a.k.a target.getHit();
+    }
 };
 
 /*
@@ -60,6 +64,17 @@ Blaze.prototype.toString = function () {
 };
 
 
-Blaze.prototype.update = function (position) {
+Blaze.prototype.update = function (controller, eggman) {
 
+    if (controller.mousePosition !== null) {
+        //  console.log(controller.mousePosition);
+        this.position = controller.mousePosition;
+    }
+
+    if(controller.mouseClick !== null)
+    {
+        this.shoot(eggman);
+        // console.log(controller.mouseClick);
+        controller.mouseClick = null;
+    }
 };
