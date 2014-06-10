29c33720c94c50219107a28aaa408604972b6254
phristov <pavelh@abv.bg>
2014-06-10 14:41:56 +0300

    Changed eggman size.

--- src/main/webapp/resources/scripts/eggman.js
+++ src/main/webapp/resources/scripts/eggman.js
@@ -5,8 +5,8 @@ function Eggman(coordinate) {
     GameObject.call(this, coordinate);
     this.onScreen = false;
     this.speed = 2;
-    this.width = 20;
-    this.height = 20;
+    this.width = 60;
+    this.height = 60;
 }
 
 /*
@@ -22,21 +22,21 @@ Eggman.prototype.constructor = Eggman;
 /*
  *   Prints the properties of eggman.
  */
-Eggman.prototype.toString = function () {
+Eggman.prototype.toString = function() {
     var result = "Eggman:\n" + GameObject.prototype.toString.call(this);
     return result;
 };
 
 // Moves eggman with own speed
-Eggman.prototype.Move = function () {
+Eggman.prototype.Move = function() {
     this.position.x += this.speed;
     this.position.y += 0;
 };
 
-Eggman.prototype.MoveTo = function (coordinate) {
+Eggman.prototype.MoveTo = function(coordinate) {
     this.position = coordinate;
 };
 
-Eggman.prototype.Hide = function () {
+Eggman.prototype.Hide = function() {
     this.onScreen = false;
 };
