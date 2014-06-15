c621f985412c5a53f89d44e820f1983bf1b5626b
phristov <pavelh@abv.bg>
2014-06-15 17:46:39 +0300

    small changes

--- src/main/webapp/resources/scripts/common.js
+++ src/main/webapp/resources/scripts/common.js
@@ -1,10 +1,13 @@
 /*
- *   Returns a random integer between min and max
+ *   Common object.
  */
-function Common(){
+function Common() {
 
 }
 
-Common.getRandomInt= function(min, max) {
+/*
+ *   Returns a random integer between min and max
+ */
+Common.getRandomInt = function (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
